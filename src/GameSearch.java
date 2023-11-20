34b55181dfbccaff0c7ede1f1091a4a39e22e035
maroua <marouaamal2002@gmail.com>
2023-11-20 11:56:05 +0100

    commit

--- src/GameSearch.java
+++ src/GameSearch.java
@@ -1,2 +1,14 @@
 abstract public class GameSearch {
+    public static final boolean DEBUG = false;
+    public static boolean PROGRAM = false;
+    public static boolean HUMAN = true;
+
+    public abstract boolean drawnPosition(Position p);
+    public abstract boolean wonPosition(Position p, boolean player);
+    public abstract float positionEvaluation(Position p, boolean player);
+    public abstract void printPosition(Position p);
+    public abstract Position [] possibleMoves(Position p, boolean player);
+    public abstract Position makeMove(Position p, boolean player, Move move);
+    public abstract boolean reachedMaxDepth(Position p, int depth);
+    public abstract Move createMove();
 }
